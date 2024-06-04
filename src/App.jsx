ad7f9afb3658d1ec8ac3de19f36ec7f5814a4e66
bosch <130572668+boschaiyadej@users.noreply.github.com>
2024-06-04 18:23:38 +0700

    refactor: change button and alert

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,24 @@
 import "./App.css";
 import Alert from "./components/Alert";
 import Button from "./components/Button";
-import ImgOctagon from "./assets/alert-octagon.png";
-import ImgTriangle from "./assets/alert-triangle.png";
-import ImgCircle from "./assets/alert-circle.png";
-import ImgCheck from "./assets/check-square.png";
 
 function App() {
-  const color = {
-    primary: "rgba(7, 78, 232, 1)",
-    secondary: "rgba(7, 164, 232, 1)",
-    error: "rgba(222, 87, 83, 1)",
-    success: "rgba(38, 183, 149, 1)",
-  };
   return (
     <div className="App">
       <div className="button-components-section">
         {/* Render ตัว Button 2 แบบ */}
-        <Button color={color["primary"]} />
-        <Button color={color["secondary"]} />
+        <Button type="primary" />
+        <Button type="secondary" />
+        <Button type="error" />
+        <Button type="success" />
       </div>
       <hr />
       <div className="alert-components-section">
         {/* Render ตัว Alert 4 แบบ */}
-        <Alert src={ImgOctagon} alt={ImgOctagon} />
-        <Alert src={ImgTriangle} alt={ImgTriangle} />
-        <Alert src={ImgCircle} alt={ImgCircle} />
-        <Alert src={ImgCheck} alt={ImgCheck} />
+        <Alert type="error" />
+        <Alert type="warning" />
+        <Alert type="info" />
+        <Alert type="success" />
       </div>
     </div>
   );
--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,6 +2,24 @@
 import { css } from "@emotion/react";
 
 function Button(props) {
+  let color = undefined;
+  switch (props.type) {
+    case "primary":
+      color = "rgba(7, 78, 232, 1)";
+      break;
+    case "secondary":
+      color = "rgba(7, 164, 232, 1)";
+      break;
+    case "error":
+      color = "rgba(222, 87, 83, 1)";
+      break;
+    case "success":
+      color = "rgba(38, 183, 149, 1)";
+      break;
+    default:
+      console.log(`Don't have type of button`);
+      break;
+  }
   return (
     <>
       <button
@@ -14,10 +32,10 @@ function Button(props) {
           padding: 0px 16px;
           color: white;
           gap: 8px;
-          background-color: ${props.color};
+          background-color: ${color};
         `}
       >
-        Button
+        {props.type}
       </button>
     </>
   );
