ebc76a3a809eb8758a053ffa45f7b42f1cb92276
PNKim <paljapon.nait@gmail.com>
2024-06-04 13:42:58 +0700

    feat: alert component

--- src/components/Alert.jsx
+++ src/components/Alert.jsx
@@ -1,3 +1,40 @@
 // Start coding here
+import "./Alert.css";
+import ErrorImage from "./images/frown.svg";
+import WarningImage from "./images/alert-triangle.svg";
+import InfoImage from "./images/alert-circle.svg";
+import SuccesImage from "./images/check-circle.svg";
 
-function Alert() {}
+function Alert(props) {
+  if (props.type === "error") {
+    return (
+      <div className={props.type}>
+        <img src={ErrorImage} alt="error" />
+        <span>This is error alert box</span>
+      </div>
+    );
+  } else if (props.type === "warning") {
+    return (
+      <div className={props.type}>
+        <img src={WarningImage} alt="warning" />
+        <span>This is warning alert box</span>
+      </div>
+    );
+  } else if (props.type === "info") {
+    return (
+      <div className={props.type}>
+        <img src={InfoImage} alt="info" />
+        <span>This is info alert box</span>
+      </div>
+    );
+  } else if (props.type === "success") {
+    return (
+      <div className={props.type}>
+        <img src={SuccesImage} alt="success" />
+        <span>This is success alert box</span>
+      </div>
+    );
+  }
+}
+
+export default Alert;
